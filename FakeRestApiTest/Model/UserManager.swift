b3afc755279dc7a383239fc938260a21d4fa9af6
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-02-15 13:27:14 +0330

    Add to git: UserManager

---
+++ FakeRestApiTest/Model/UserManager.swift
@@ -0,0 +1,40 @@
+//
+//  UserManager.swift
+//  FakeRestApiTest
+//
+//  Created by Omid Heydarzadeh on 2/15/24.
+//
+
+// https://jsonplaceholder.typicode.com/posts
+
+import Foundation
+
+struct UserManager {
+    
+    func fetch() {
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        
+        let dataTask = URLSession.shared.dataTask(with: url) { data, _, error in
+            
+            if let error = error { print("there is an error for fetching: \(error.localizedDescription)"); return }
+            
+            guard let data = data else { return }
+            
+            let decoder = JSONDecoder()
+            
+            do {
+                let decodedData = try decoder.decode([UserModel].self, from: data)
+                print(decodedData)
+            } catch {
+                print("Error for decoding data: \(error.localizedDescription)")
+            }
+            
+        }
+       
+        dataTask.resume()
+        
+    }
+    
+}
+
