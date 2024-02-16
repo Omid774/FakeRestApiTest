54dc40a437ea72a63e2cc49e474fa37cfb9b5112
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-02-16 18:31:07 +0330

    playground: check and receive image.

--- FakeRestApiTest/Playground/Practice.playground/Contents.swift
+++ FakeRestApiTest/Playground/Practice.playground/Contents.swift
@@ -11,12 +11,14 @@ var greeting = "Hello, playground"
 // MARK: - Comments
 
 /*
-    playground: check data is contain image or not.
+    playground: check and receive image.
  */
 
-func fetch() {
+let myURL = "https://www.save-free.com/cdn/https://scontent-bcn1-1.cdninstagram.com/v/t51.2885-19/365431917_815678320106123_5690402445351449711_n.jpg?_nc_ht=scontent-bcn1-1.cdninstagram.com&_nc_cat=111&_nc_ohc=NnSqKtcEaFUAX8lj6JV&edm=AEF8tYYBAAAA&ccb=7-5&oh=00_AfDxeoe99-fBJCi7eLeJteFKBbt7B05yyh1n0NtppbnOAA&oe=65D3E803&_nc_sid=1e20d2"
+
+func fetch() -> Bool {
     
-    guard let url = URL(string: "https://reactnative.dev/movies.json") else { return }
+    guard let url = URL(string: myURL) else { return false }
     
     let dataTask = URLSession.shared.dataTask(with: url) { data, _, error in
         
@@ -24,17 +26,14 @@ func fetch() {
         
         guard let data = data else { return }
         
-        print(data)
+        guard let imageData = UIImage(data: data) else { return }
         
-        if let image = UIImage(data: data) { print("this data contain image.") } else {
-            print("NOT IMAGE DATA")
-        }
-        
-        print("END")
+        print(imageData)
         
     }
     
     dataTask.resume()
+    return true
 }
 
 fetch()
